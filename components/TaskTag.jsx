71a54a489f056a819f3440aee031e7f8d9c4c881
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-12-11 18:47:25 +0100

    fixed errors in TagTask

--- components/TaskTag.jsx
+++ components/TaskTag.jsx
@@ -1,70 +1,68 @@
 import tasks from "../models/tasks";
 
-function TaskTag() {
-    let tag = tasks.map((task) => {
-        if (task.state === 'completed') {
-            return (
-                <span
-                    key={task.id}
-                    style={{
-                        backgroundColor: 'green',
-                        color: 'white',
-                        padding: '5px 10px',
-                        borderRadius: '5px',
-                        fontWeight: 'bold',
-                    }}
-                >
-                    Completed
-                </span>
-            );
-        } else if (task.state === 'in_progress') {
-            return (
-                <span
-                    key={task.id}
-                    style={{
-                        backgroundColor: 'blue',
-                        color: 'white',
-                        padding: '5px 10px',
-                        borderRadius: '5px',
-                        fontWeight: 'bold',
-                    }}
-                >
-                    In progress
-                </span>
-            );
-        } else if (task.state === 'backlog') {
-            return (
-                <span
-                    key={task.id}
-                    style={{
-                        backgroundColor: 'red',
-                        color: 'white',
-                        padding: '5px 10px',
-                        borderRadius: '5px',
-                        fontWeight: 'bold',
-                    }}
-                >
-                    Backlog
-                </span>
-            );
-        } else {
-            return (
-                <span
-                    key={task.id}
-                    style={{
-                        backgroundColor: 'grey',
-                        color: 'white',
-                        padding: '5px 10px',
-                        borderRadius: '5px',
-                        fontWeight: 'bold',
-                    }}
-                >
-                    Undefined State
-                </span>
-            );
-        }
-    })
-    return <div>{tag}</div>;
+function TaskTag({ task }) {
+
+    if (tasks.state === 'completed') {
+        return (
+            <span
+                key={tasks.id}
+                style={{
+                    backgroundColor: 'green',
+                    color: 'white',
+                    padding: '5px 10px',
+                    borderRadius: '5px',
+                    fontWeight: 'bold',
+                }}
+            >
+                Completed
+            </span>
+        );
+    } else if (tasks.state === 'in_progress') {
+        return (
+            <span
+                key={tasks.id}
+                style={{
+                    backgroundColor: 'blue',
+                    color: 'white',
+                    padding: '5px 10px',
+                    borderRadius: '5px',
+                    fontWeight: 'bold',
+                }}
+            >
+                In progress
+            </span>
+        );
+    } else if (tasks.state === 'backlog') {
+        return (
+            <span
+                key={tasks.id}
+                style={{
+                    backgroundColor: 'red',
+                    color: 'white',
+                    padding: '5px 10px',
+                    borderRadius: '5px',
+                    fontWeight: 'bold',
+                }}
+            >
+                Backlog
+            </span>
+        );
+    } else {
+        return (
+            <span
+                key={tasks.id}
+                style={{
+                    backgroundColor: 'grey',
+                    color: 'white',
+                    padding: '5px 10px',
+                    borderRadius: '5px',
+                    fontWeight: 'bold',
+                }}
+            >
+                Undefined State
+            </span>
+        );
+    }
 }
 
 export default TaskTag;
