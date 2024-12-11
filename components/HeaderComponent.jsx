9ed03e723ed6c85a3ca6b3ef4b3926560e271402
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-12-11 15:48:10 +0100

    created HeaderComponent

---
+++ components/HeaderComponent.jsx
@@ -0,0 +1,9 @@
+function HeaderComponent() {
+    return (
+        <header>
+            <h1>Task Manager</h1>
+        </header>
+    )
+}
+
+export default HeaderComponent;
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import tasks from '../models/tasks'
+import HeaderComponent from '../components/HeaderComponent';
 
 function App() {
+  // parte logica
 
-
+  // html
   return (
     <>
+      <HeaderComponent />
+      <main>
+        <h2>Current tasks ({currentTasks.length})</h2>
+        <ul>
 
+        </ul>
+      </main>
     </>
   )
 }
